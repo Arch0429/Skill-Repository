854d9e03e447be637d899aa73b216206615c4282
JSYOU <jsgod96@naver.com>
2025-09-25 12:40:47 +0900

    수정

--- study/react/origin/src/App.jsx
+++ study/react/origin/src/App.jsx
@@ -1,15 +1,62 @@
 import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <main>
+          <div class="card">
+              <div class="card__header">강의 목록</div>
+              <div class="card__body">
+                  <div class="courses">
+                      <article class="course">
+                          <img
+                              class="course__img"
+                              src="../public/img/htmlcss.png"
+                              alt=""
+                          />
+                          <div class="course__body">
+                              <div class="course__title">
+                                  입문자를 위한, HTML&CSS 웹 개발 입문
+                              </div>
+                              <div class="course__description">
+                                  웹 개발에 필요한 기본 지식을 배웁니다.
+                              </div>
+                          </div>
+                      </article>
+                      <article class="course">
+                          <img
+                              class="course__img"
+                              src="../public/img/htmlcss.png"
+                              alt=""
+                          />
+                          <div class="course__body">
+                              <div class="course__title">
+                                  입문자를 위한, HTML&CSS 웹 개발 입문
+                              </div>
+                              <div class="course__description">
+                                  웹 개발에 필요한 기본 지식을 배웁니다.
+                              </div>
+                          </div>
+                      </article>
+                      <article class="course">
+                          <img
+                              class="course__img"
+                              src="../public/img/htmlcss.png"
+                              alt=""
+                          />
+                          <div class="course__body">
+                              <div class="course__title">
+                                  입문자를 위한, HTML&CSS 웹 개발 입문
+                              </div>
+                              <div class="course__description">
+                                  웹 개발에 필요한 기본 지식을 배웁니다.
+                              </div>
+                          </div>
+                      </article>
+                  </div>
+              </div>
+          </div>
+      </main>    
     </>
   )
 }
---
+++ study/react/origin/src/AppCounter.jsx
@@ -0,0 +1,17 @@
+import './AppCounter.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
