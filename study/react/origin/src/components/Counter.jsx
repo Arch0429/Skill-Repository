deeb65977d298696817de3e69d76692a3de5839f
JSYOU <jsgod96@naver.com>
2025-09-30 12:46:28 +0900

    추가

--- study/react/origin/src/components/Counter.jsx
+++ study/react/origin/src/components/Counter.jsx
@@ -1,6 +1,15 @@
-function Counter() {
+import { useState } from "react";
+
+function Counter({ onTotal }) {
+  let [counter, setCounter] = useState(0);
+
+  function handleCounter() {
+    setCounter(counter + 1);
+    onTotal();
+  }
+
   return (
-    <button>Counter</button>
+    <button onClick={handleCounter}>Counter : {counter}</button>
   )
 }
 export default Counter;
