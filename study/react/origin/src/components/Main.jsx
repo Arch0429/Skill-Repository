cdc30c6c9032fee08e6aa22c9b45173a73bd31ce
JSYOU <jsgod96@naver.com>
2025-09-30 22:15:24 +0900

    추가

--- study/react/origin/src/components/Main.jsx
+++ study/react/origin/src/components/Main.jsx
@@ -3,6 +3,7 @@ import Counter from "./Counter";
 
 function Main() {
   const [total, setTotal] = useState(0);
+  const [flag, setFlag] = useState(false);
 
   const handleTotal = () => {
     setTotal(total + 1);
@@ -11,6 +12,10 @@ function Main() {
   return (
     <main>
       <h2>Total : {total}</h2>
+      <h2>Flag : {flag.toString()}</h2>
+      <button onClick={() => {
+        setFlag(!flag);
+      }}>Toggle Flag</button>
       <Counter onTotal={handleTotal}/>
       <br/>
       <br/>
